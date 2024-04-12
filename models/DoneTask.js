b4d16462f6c4366d84973276e74cbef4bf3cd7dc
josepCortesManyanich <gaara96@gmail.com>
2024-04-12 16:52:53 +0200

    models solved

---
+++ models/DoneTask.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongeso')
+const { Schema, model } = mongoose;
+
+const doneSchema = new Schema({
+  quantity: {
+    type: Number,
+    unique: true,
+    required: true
+  },
+ 
+  task: {
+    type: [Schema.Types.ObjectId],
+    ref: 'Task'  
+  }
+},
+  {
+    timestamps: true
+  });
+
+module.exports = model("Task", taskSchema);
