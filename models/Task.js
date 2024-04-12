6d5b07c63c0c8f9fefcbde39cd855dd3ae9105d8
josepCortesManyanich <gaara96@gmail.com>
2024-04-12 16:49:00 +0200

    IP

---
+++ models/Task.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const taskSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  prioridad: {
+    type: Number,
+    required: true
+  },
+  fecha: {
+    type: String,
+    required: true
+  },
+   description: {
+    type: String,
+    required: true
+  },
+  propiedad: {
+    type: String,
+    enum: ['Raquel', 'Josep'],
+  }
+},
+  {
+    timestamps: true
+  });
+
+module.exports = model("Task", taskSchema);
